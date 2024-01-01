c4d199c06283aebc17b8150cedaae4223a2ef8de
Leidi <leididelapuente3@gmail.com>
2024-01-01 01:57:39 -0500

    style(add styles to the user card): added responsivness for tablet and desktop

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import iconMoon from './assets/img/icon-moon.svg'
 import iconSearch from './assets/img/icon-search.svg'
 function App() {
 	const user = {
-		avatar_url: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSKItrmteWSpbTNV6PLo7_Gbt6igSgwY-Mj2pPwSDbLYQ9Gi59Q",
+		avatar_url: "https://i.pinimg.com/originals/88/f9/7b/88f97b667be99a794e956f8440ea60f2.jpg",
 		name: "monalisa octocat",
         login: "octocat",
         created_at: "1-30-2020",
@@ -17,14 +17,14 @@ function App() {
         company: "GitHub"
 	}
 	return (
-		<main className="px-3 py-4  flex flex-col justify-items-center max-w-2xl w-11/12 my-4 mx-auto">
+		<main className="px-3 py-4  flex flex-col justify-items-center max-w-xl w-full my-4 mx-auto lg:max-w-2xl">
             <header className="flex flex-row justify-between items-center mb-5">
                 <h2 className="text-github-300 text-3xl font-bold">devfinder</h2>
-                <button className="text-github-300 uppercase">Light <img src={iconMoon}/></button>
+                <button className="text-github-300 uppercase font-bold">Dark <img src={iconMoon}/></button>
             </header>
             <div className="bg-white-100 mb-4 px-3 py-2 flex gap-2 light-shadow rounded-lg">
                 <img src={iconSearch} alt="" className="w-5 h-5 self-center"/>
-                <input type="text" placeholder= "Search GitHub username..." className="flex-auto outline-none text-github-300 placeholder:text-github-300"/>
+                <input type="text" placeholder= "Search GitHub username..." className="flex-auto outline-none text-github-300 placeholder:text-github-300 text-lg"/>
                 <button type="button" className="bg-github-200 h-10 w-24 py-1 px-3 text-white-100 rounded-lg font-bold hover:bg-github-100">Search</button>
             </div>
 			<UserCard user={user}/>
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -7,12 +7,12 @@ import PropTypes from "prop-types";
 
 export const UserCard = ({ user }) => {
   return (
-    <div className="light-shadow rounded-xl bg-white-100 py-3 px-4">
-      <div className="mb-2 flex items-center gap-2 py-1">
+    <div className="light-shadow rounded-xl bg-white-100 px-5 py-4">
+      <div className="mb-2 flex items-center gap-6 py-1 lg:gap-x-8">
         <img
           src={user.avatar_url}
           alt="icon"
-          className="w-2/12 max-w-16 rounded-full"
+          className="w-3/12 max-w-28 rounded-full"
         />
         <div>
           <p className="text-lg font-semibold text-github-600">{user.name}</p>
@@ -20,28 +20,33 @@ export const UserCard = ({ user }) => {
           <p>{`Joined ${user.created_at}`}</p>
         </div>
       </div>
-      <p className="my-2">{user.bio}</p>
-      <div className="bg-white-200 grid grid-cols-3 place-items-center rounded-lg px-3 py-4 my-4">
+      <p className="my-2 lg:ml-36">{user.bio}</p>
+      <div className="w-full my-4 grid grid-cols-3 place-items-center rounded-lg bg-white-200 px-3 py-4 lg:w-3/4 lg:ml-36">
         <p>Repositories</p>
         <p>Followers</p>
         <p>Following</p>
-        <p className="text-github-600 font-bold">{user.public_repos}</p>
-        <p className="text-github-600 font-bold">{user.followers}</p>
-        <p className="text-github-600 font-bold">{user.following}</p>
+        <p className="font-bold text-github-600 text-xl">{user.public_repos}</p>
+        <p className="font-bold text-github-600 text-xl">{user.followers}</p>
+        <p className="font-bold text-github-600 text-xl">{user.following}</p>
       </div>
-      <div className="leading-9">
-        <p>
-          <img src={iconLocation} alt="location icon" /> {user.location}
-        </p>
-        <a href={user.blog} target="_blank" rel="noreferrer">
-          <img src={iconWebsite} alt="link icon" /> {user.blog}
-        </a>
-        <p>
-          <img src={iconTwitter} alt="twitter icon" /> {user.twitter_username}
-        </p>
-        <p>
-          <img src={iconCompany} alt="building icon" /> {user.company}
-        </p>
+      <div className="leading-9 flex flex-col md:flex-row md:gap-24 md:justify-center lg:ml-28
+       lg:w-50 lg:gap-10">
+        <div>
+          <p>
+            <img src={iconLocation} alt="location icon" /> {user.location}
+          </p>
+          <a href={user.blog} target="_blank" rel="noreferrer">
+            <img src={iconWebsite} alt="link icon" /> {user.blog}
+          </a>
+        </div>
+        <div>
+          <p>
+            <img src={iconTwitter} alt="twitter icon" /> {user.twitter_username}
+          </p>
+          <p>
+            <img src={iconCompany} alt="building icon" /> {user.company}
+          </p>
+        </div>
       </div>
     </div>
   );
