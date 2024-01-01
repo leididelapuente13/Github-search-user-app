020b9f3778d9e2cf165fe827e358d9e1696ffdf7
Leidi <leididelapuente3@gmail.com>
2023-12-31 20:12:10 -0500

    feat: added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { UserCard } from "./components/UserCard";
-
+import iconMoon from './assets/img/icon-moon.svg'
+import iconSearch from './assets/img/icon-search.svg'
 function App() {
 	const user = {
-		avatar_url: "https://github.com/images/error/octocat_happy.gif",
+		avatar_url: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSKItrmteWSpbTNV6PLo7_Gbt6igSgwY-Mj2pPwSDbLYQ9Gi59Q",
 		name: "monalisa octocat",
         login: "octocat",
         created_at: "1-30-2020",
@@ -16,9 +17,18 @@ function App() {
         company: "GitHub"
 	}
 	return (
-		<div>
+		<main className="px-3 py-4  flex flex-col justify-items-center max-w-2xl w-11/12 my-4 mx-auto">
+            <header className="flex flex-row justify-between items-center mb-5">
+                <h2 className="text-github-300 text-3xl font-bold">devfinder</h2>
+                <button className="text-github-300 uppercase">Light <img src={iconMoon}/></button>
+            </header>
+            <div className="bg-white-100 mb-4 px-3 py-2 flex gap-2 light-shadow rounded-lg">
+                <img src={iconSearch} alt="" className="w-5 h-5 self-center"/>
+                <input type="text" placeholder= "Search GitHub username..." className="flex-auto outline-none text-github-300 placeholder:text-github-300"/>
+                <button type="button" className="bg-github-200 h-10 w-24 py-1 px-3 text-white-100 rounded-lg font-bold hover:bg-github-100">Search</button>
+            </div>
 			<UserCard user={user}/>
-		</div>
+		</main>
 	);
 }
 export default App;
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,48 +3,63 @@ import iconLocation from "../assets/img/icon-location.svg";
 import iconTwitter from "../assets/img/icon-twitter.svg";
 import iconWebsite from "../assets/img/icon-website.svg";
 
-import PropTypes from 'prop-types'; 
+import PropTypes from "prop-types";
 
 export const UserCard = ({ user }) => {
   return (
-    <div>
-      <div>
-        <img src={user.avatar_url} alt="" />
+    <div className="light-shadow rounded-xl bg-white-100 py-3 px-4">
+      <div className="mb-2 flex items-center gap-2 py-1">
+        <img
+          src={user.avatar_url}
+          alt="icon"
+          className="w-2/12 max-w-16 rounded-full"
+        />
         <div>
-          <p>{user.name}</p>
-          <p>{user.login}</p>
+          <p className="text-lg font-semibold text-github-600">{user.name}</p>
+          <p className="text-github-200">{user.login}</p>
           <p>{`Joined ${user.created_at}`}</p>
         </div>
       </div>
-      <p>{user.bio}</p>
-      <div>
-        <p>{user.public_repos}</p>
-        <p>{user.followers}</p>
-        <p>{user.following}</p>
+      <p className="my-2">{user.bio}</p>
+      <div className="bg-white-200 grid grid-cols-3 place-items-center rounded-lg px-3 py-4 my-4">
+        <p>Repositories</p>
+        <p>Followers</p>
+        <p>Following</p>
+        <p className="text-github-600 font-bold">{user.public_repos}</p>
+        <p className="text-github-600 font-bold">{user.followers}</p>
+        <p className="text-github-600 font-bold">{user.following}</p>
       </div>
-      <div>
-        <p><img src={iconLocation} alt="location icon" />{user.location}</p>
-        <a href={user.blog} target="_blank" rel="noreferrer"><img src={iconWebsite} alt="link icon" />{user.blog}</a>
-        <p><img src={iconTwitter} alt="twitter icon" /> {user.twitter_username}</p>
-        <p><img src={iconCompany} alt="building icon" /> {user.company}</p>
+      <div className="leading-9">
+        <p>
+          <img src={iconLocation} alt="location icon" /> {user.location}
+        </p>
+        <a href={user.blog} target="_blank" rel="noreferrer">
+          <img src={iconWebsite} alt="link icon" /> {user.blog}
+        </a>
+        <p>
+          <img src={iconTwitter} alt="twitter icon" /> {user.twitter_username}
+        </p>
+        <p>
+          <img src={iconCompany} alt="building icon" /> {user.company}
+        </p>
       </div>
     </div>
   );
 };
 
 UserCard.propTypes = {
-    user: PropTypes.shape({
-        avatar_url: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        login: PropTypes.string.isRequired,
-        created_at: PropTypes.string.isRequired,
-        bio: PropTypes.string,
-        public_repos: PropTypes.number,
-        followers: PropTypes.number.isRequired,
-        following: PropTypes.number.isRequired,
-        location: PropTypes.string,
-        blog: PropTypes.string,
-        twitter_username: PropTypes.string,
-        company: PropTypes.string
-    })
-}
+  user: PropTypes.shape({
+    avatar_url: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    login: PropTypes.string.isRequired,
+    created_at: PropTypes.string.isRequired,
+    bio: PropTypes.string,
+    public_repos: PropTypes.number,
+    followers: PropTypes.number.isRequired,
+    following: PropTypes.number.isRequired,
+    location: PropTypes.string,
+    blog: PropTypes.string,
+    twitter_username: PropTypes.string,
+    company: PropTypes.string,
+  }),
+};
