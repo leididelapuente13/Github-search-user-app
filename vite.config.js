b23afb4befea6d15630e913db2bfc04805bdc89b
Leidi <leididelapuente3@gmail.com>
2024-01-07 17:43:53 -0500

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Github-search-user-app/'
+  base: process.env.NODE_ENV === 'production' ? '/Github-search-user-app/' : '/',
 })
